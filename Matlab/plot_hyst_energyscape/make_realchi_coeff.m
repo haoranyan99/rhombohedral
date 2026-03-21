265eb13a9e37086d763d6c767cb4cd446f45bd07
hyan318 <hyan318@gatech.edu>
2026-03-21 15:22:45 -0400

    update_3_21

--- Matlab/plot_hyst_energyscape/make_realchi_coeff.m
+++ Matlab/plot_hyst_energyscape/make_realchi_coeff.m
@@ -3,18 +3,23 @@ function coef = make_realchi_coeff(par)
     coef = struct();
     c2 = 1; % c2 > 0 to ensure F>0 when X is large
 
-    % new def: T = A * dop^2 + Tc for lattice phase boundary
+    % new def: T = -A * dop^2 + C for lattice phase boundary
     dop1 = par.Lat_pt1(1);
     T1 = par.Lat_pt1(2);
     dop2 = par.Lat_pt2(1);
     T2 = par.Lat_pt2(2);
-    A = (T2 - T1) / (dop2*dop2 - dop1*dop1);
-    Tc = (T1*dop2*dop2 - T2*dop1*dop1) / (dop2*dop2 - dop1*dop1);
+    A = (T1 - T2) / (dop2*dop2 - dop1*dop1);
+    C = (T1*dop2*dop2 - T2*dop1*dop1) / (dop2*dop2 - dop1*dop1);
     
+
+    % calculate b2 from theoretical boundary 5/6 * b2^2 - a2 * c2 = 0
+
     % a2 = alpha * (T - Tc)
-    alpha = 5 / (6 * A * c2);
+    alpha = par.Lat_alpha;
+    Tc = par.Lat_Tc;
+
     coef.a2 = @(T) alpha * (T-Tc);
-    coef.b2 = @(dop) abs(dop);
+    coef.b2 = @(dop) sqrt(1.2 * c2 * alpha * (-A * dop * dop + C - Tc));
     coef.c2 = c2;
 
     coef.eval = @(T,dop,chi_used) eval_all_(par, coef, T, dop, chi_used);
--- Matlab/plot_hyst_energyscape/make_realchi_params.m
+++ Matlab/plot_hyst_energyscape/make_realchi_params.m
@@ -34,9 +34,15 @@ function par = make_realchi_params(noAsk)
     par.dop_span = 1.0;
 
     % =================== define pt1(dop1, T1), pt2(dop2, T2) on the lattice trans boundary ==============
+    % =================== T = @(dop) -A * dop^2 + C for lattice mode boundary (A,C detemined in coeff.m) ====================
     par.Lat_pt1 = [-3, 5];
-    par.Lat_pt2 = [-2.8, 10];
-
+    par.Lat_pt2 = [-2.7, 8];
+    
+    
+    % =================== a2 = alpha * (T - Tc) is predefined.(alpha > 0) ====================
+    par.Lat_Tc = 10;
+    par.Lat_alpha = 5e-3;
+    
 
     % =================== artificial magnetic field (meV) =========
     par.artificial_polar = 0; 
